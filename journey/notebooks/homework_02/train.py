85e1c20277c5850f1a3f41d65790dbac7d8aac68
Carlos A. Crespo <lvccrespo@gmail.com>
2022-07-10 00:36:16 -0300

    homework_02

---
+++ journey/notebooks/homework_02/train.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+import pickle
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+import mlflow
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+def run(data_path):
+
+    with mlflow.start_run():
+
+        mlflow.set_tag("developer", "ebo1")
+        mlflow.autolog()
+
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+
+#        mlflow.log_param("train-data-path", os.path.join(data_path, "train.pkl"))
+#        mlflow.log_param("val-data-path", os.path.join(data_path, "valid.pkl"))
+
+        alpha = 0.01
+#        mlflow.log_param("alpha", alpha)
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+#        mlflow.log_metric("rmse", rmse)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path)
